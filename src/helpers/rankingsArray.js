75ea9aae4ca778a184524e551497b33a651cc7c4
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-24 18:13:54 +0200

    [WIP] Create `rankingsArray` for display

---
+++ src/helpers/rankingsArray.js
@@ -0,0 +1,5 @@
+export const rankingsArray = [
+  { value: "230+", text: "Scientifically based tests" },
+  { value: "10,000+", text: "Medical recommendations" },
+  { value: "25M+", text: "Students learning" },
+];
