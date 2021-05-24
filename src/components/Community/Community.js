f7ace2346d59cd1ccd5a67ee24052a3f60f01649
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-24 18:14:33 +0200

    [WIP]

---
+++ src/components/Community/Community.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Community.scss";
+
+const Community = () => {
+  return (
+    <div className="container-fluid community">
+      <div className="community__wrapper text-md-center">
+        <h3 className="community__title">What our community is saying?</h3>
+        <p className="community__text">A mission-driven company that invest in and builds healthier living</p>
+      </div>
+    </div>
+  );
+};
+
+export default Community;
