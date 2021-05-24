5be8e40636d4c5e75216f9c13d18cbeb05f9a71c
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-24 19:16:09 +0200

    [WIP] Fix imports and data

--- src/helpers/profitsArray.js
+++ src/helpers/profitsArray.js
@@ -1,7 +1,7 @@
 import { Book, Chat, Garden } from "../assets/images/profits";
 
-export const rankingsArray = [
-  { image: Book, title: "1 FREE chat session", text: "Start with a free text based chat session online" },
-  { image: Chat, title: "Personal Coach", text: "A dedicated coach to help you through your journey" },
+export const profitsArray = [
+  { image: Chat, title: "1 FREE chat session", text: "Start with a free text based chat session online" },
+  { image: Book, title: "Personal Coach", text: "A dedicated coach to help you through your journey" },
   { image: Garden, title: "Text chat session", text: "An excellent way of preaparing a projects wireframe" },
 ];
