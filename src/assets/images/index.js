2a0dd2f861b6da0ac96e2d4fff904ddcb0731e7f
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-24 19:41:23 +0200

    [WIP] Create new logo image file

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import Hero from "./hero.svg";
 import Logo from "./logo.svg";
 import LogoText from "./logo-text.svg";
+import LogoWhite from "./logo-white.svg";
 
-export { Hero, Logo, LogoText };
+export { Hero, Logo, LogoText, LogoWhite };
