a376f33ec7dce3a55fc7f9c5f759f6c95c3b1ad3
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-24 18:12:43 +0200

    [WIP] Import components

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,11 +1,19 @@
 import React from "react";
+
+import Community from "../components/Community/Community";
+import Header from "../components/Header/Header";
 import NavBar from "../components/NavBar/NavBar";
+import Rankings from "../components/Rankings/Rankings";
+
 import "./Root.scss";
 
 const Root = () => {
   return (
     <div>
       <NavBar />
+      <Header />
+      <Rankings />
+      <Community />
     </div>
   );
 };
