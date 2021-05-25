835b1091d0fef861ed0ace2eeea5fe3e5961532e
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-25 14:45:20 +0200

    [WIP] Get play icon from FontAwesome

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,7 +3,6 @@ import Logo from "./logo.png";
 import LogoDot from "./logo-dot.svg";
 import LogoText from "./logo-text.svg";
 import LogoWhite from "./logo-white.png";
-import Play from "./play.svg";
 import Video from "./video_image.png";
 
-export { Hero, Logo, LogoDot, LogoText, LogoWhite, Play, Video };
+export { Hero, Logo, LogoDot, LogoText, LogoWhite, Video };
--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -7,7 +7,8 @@ const Community = () => {
     <div className="container-fluid community">
       <div className="community__wrapper text-md-center">
         <h3 className="community__title">What our community is saying?</h3>
-        <p className="community__text">A mission-driven company that invest in and builds healthier living</p>
+        <p className="community__text">A mission-driven company that invest</p>
+        <p className="community__text">in and builds healthier living</p>
         <div className="container community__movie">
           <img src={Video} alt="Lady" className="community__movie__image"/>
         </div>
